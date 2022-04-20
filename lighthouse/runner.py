d40da18e108af8a2095131f643074d2453fab54b
f02h <staric.david@gmail.com>
2022-04-20 13:24:55 +0200

    Update runner.py

--- lighthouse/runner.py
+++ lighthouse/runner.py
@@ -51,8 +51,7 @@ class LighthouseRunner(object):
                 url,
                 '--quiet' if quiet else '',
                 '--chrome-flags="--headless"',
-                '--preset=full',
-                '--emulated-form-factor={0}'.format(form_factor),
+                '--preset=desktop',
                 '--output=json',
                 '--output-path={0}'.format(report_path),
             ]
