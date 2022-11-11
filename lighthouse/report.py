979ea62fd576fbbdc79c1abec318a940a547ffd0
Beka Lewis <bekabyx@gmail.com>
2022-11-11 11:07:42 +0000

    Fix failed audits to handle none type scores

--- lighthouse/report.py
+++ lighthouse/report.py
@@ -97,7 +97,7 @@ class LighthouseReport(object):
                     'display': v.get('displayValue'),
                 })
                 for k, v in all_audits.items()
-                if v.get('score', 0) < 1 and
+                if (v.get('score', 0) or 0) < 1 and
                 v.get('scoreDisplayMode') not in sdm_to_reject
             ]
 
